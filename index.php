7d3ddc7c4d4a3373ee39b2ea03f7485bdb97ecf4
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-02-02 18:20:43 +0100

    working on milestone1

--- index.php
+++ index.php
@@ -70,5 +70,41 @@ $dataBase = [
         "genre" => "Pop",
         "year" => "1987"
     ]
-]
+];
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- google font -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400&display=swap" rel="stylesheet">
+        <!-- style.css -->
+        <link rel="stylesheet" href="./dist/css/app.css">
+        <title>php-ajax-dischi</title>
+    </head>
+    <body>
+        <div id="app">
+            <header>
+                <div class="top_bar">
+                    <img src="./dist/img/logo.png" alt="logo">
+                </div>
+            </header>
+            <main>
+                <div class="cds_box">
+                    <?php foreach ($dataBase as $value) { ?>
+                        <div class="cds_container">
+                            <h3> <?php echo $value['title']; ?></h3>
+                            <p class="author"><?php echo $value['author']; ?></p>
+                            <p class="year"><?php echo $value['year']; ?></p>   
+                        </div>
+                    <?php } ?>
+                </div>
+            </main>
+        </div>
+    </body>
+</html>
