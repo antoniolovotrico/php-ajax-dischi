953bf9b8e729f45671ad4c825a5ea4e9525bf547
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-02-02 18:48:58 +0100

    milestone completed

--- index.php
+++ index.php
@@ -98,6 +98,7 @@ $dataBase = [
                 <div class="cds_box">
                     <?php foreach ($dataBase as $value) { ?>
                         <div class="cds_container">
+                            <img src="<?php echo $value['poster']; ?>" alt="">
                             <h3> <?php echo $value['title']; ?></h3>
                             <p class="author"><?php echo $value['author']; ?></p>
                             <p class="year"><?php echo $value['year']; ?></p>   
